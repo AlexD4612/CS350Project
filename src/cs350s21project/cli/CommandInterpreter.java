d726cbae4bd5769521de3cb5fc7758dba0f2ed1c
AlexD4612 <59583079+AlexD4612@users.noreply.github.com>
2021-05-17 13:10:26 -0700

    added initial files

---
+++ src/cs350s21project/cli/CommandInterpreter.java
@@ -0,0 +1,21 @@
+package cs350s21project.cli;
+
+public class CommandInterpreter {
+
+	
+	public void evaluate(String commandText) {
+		commandText=commandText.toLowerCase(); // lower case to allow any input in weird casing
+		commandText.trim(); //trim all whitespace
+		switch(commandText) {
+		
+		//case "command":
+			//code;
+			//break;
+		//case "different command":
+			//code
+			//break;
+		}
+	}
+	
+	
+}
