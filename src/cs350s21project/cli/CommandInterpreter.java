9d4b3b11ee176dcbae00d39bc40340b47eac5245
Ryley Horton <rhorton5@eagles.ewu.edu>
2021-05-18 16:02:56 -0700

    Minor edits
    SetLatitude() and setLongitude() just require the str to be passed rather than splitting it.
    
    Moved some of the parameters for commands since some commands may have different arguments that are placed.

--- src/cs350s21project/cli/CommandInterpreter.java
+++ src/cs350s21project/cli/CommandInterpreter.java
@@ -8,21 +8,24 @@ import cs350s21project.datatype.Longitude;
 public class CommandInterpreter {
 
 	
-	private Latitude setLatitude(String [] latData) {
+	private Latitude setLatitude(String str) {
+		String [] latData = str.split("[*#']");
 		return new Latitude(Integer.parseInt(latData[0]), Integer.parseInt(latData[1]),Double.parseDouble(latData[2]));
 	}
 	
-	private Longitude setLongitude(String [] lonData) {
+	private Longitude setLongitude(String str) {
+		String [] lonData = str.split("[*#']");
 		return new Longitude(Integer.parseInt(lonData[0]), Integer.parseInt(lonData[1]),Double.parseDouble(lonData[2]));
 	}
 	
 	public void evaluate(String commandText) {
+		String originalCommandText = commandText;
 		commandText=commandText.toLowerCase(); // lower case to allow any input in weird casing
 		commandText.trim(); //trim all whitespace
 		ArrayList <String> argumentList = new ArrayList<>();
-		while(!commandText.isEmpty()) {//Split the commandText based on whitespaces inside the string to allow for easier access to different kinds of 
-			String temp =(commandText.contains(" ")) ? commandText.substring(0,commandText.indexOf(" ")+1).trim():commandText;
-			temp = temp.replaceAll("[()]", ""); //Remove excess parameters
+		while(!commandText.isEmpty()) {//Splits the string based on whitespaces.
+			String temp =(commandText.contains(" ")) ? commandText.substring(0,commandText.indexOf(" ")+1).trim():commandText; //indexOf(" ")+1 will remove the whitespace at the start of the string.
+			temp = temp.replaceAll("[()]", ""); //Remove excess parameters.
 			argumentList.add(temp);
 			commandText = (commandText.contains(" ")) ? commandText.substring(commandText.indexOf(" ")+1).trim() : "";
 		}
@@ -33,20 +36,20 @@ public class CommandInterpreter {
 		switch(commandType) {	
 		case "create":
 			String objectType = argumentList.get(1);
-			AgentID id = new AgentID(argumentList.get(2));
-			String viewType = argumentList.get(3);
 			switch(objectType) {
 			case "window":
+				AgentID id = new AgentID(argumentList.get(2));
+				String viewType = argumentList.get(3);
 				if(viewType.equals("top")) {
 					//index 4 and 5 are skipped since they hold arbitrary values ('with','view')
 					int size = Integer.parseInt(argumentList.get(6));
-					Latitude latOrigin = setLatitude(argumentList.get(7).split("[*#']"));
-					Latitude latExtent = setLatitude(argumentList.get(8).split("[*#']"));
-					Latitude latInterval = setLatitude(argumentList.get(9).split("[*#']"));
-					Longitude longOrigin = setLongitude(argumentList.get(10).split("[*#']"));
-					Longitude longExtent = setLongitude(argumentList.get(11).split("[*#']"));
-					Longitude longInterval = setLongitude(argumentList.get(12).split("[*#']"));
-					cmd.schedule(new CommandViewCreateWindowTop(cmd,"Insert Message Here",id,size,latOrigin,latExtent,latInterval,longOrigin,longExtent,longInterval));
+					Latitude latOrigin = setLatitude(argumentList.get(7));
+					Latitude latExtent = setLatitude(argumentList.get(8));
+					Latitude latInterval = setLatitude(argumentList.get(9));
+					Longitude longOrigin = setLongitude(argumentList.get(10));
+					Longitude longExtent = setLongitude(argumentList.get(11));
+					Longitude longInterval = setLongitude(argumentList.get(12));
+					cmd.schedule(new CommandViewCreateWindowTop(cmd,originalCommandText,id,size,latOrigin,latExtent,latInterval,longOrigin,longExtent,longInterval));
 				}else if(viewType.equals("front")) {
 					System.out.println("Front view accessed, but not yet ready.");
 				}else if(viewType.equals("side")) {
@@ -54,21 +57,11 @@ public class CommandInterpreter {
 				
 				}else 
 					throw new RuntimeException("Invalid view type for create window.");
-			break;
+				break;
 				
-			}
-			
-			
-		}
-			
-		//case "command":
-				//code;
-				//break;
-			//case "different command":
-				//code
-				//break;
-			
-	}
+			}//End of switch(objectType)	
+		}//End of switch(commandType)
+	}//End of evaluate()
 }
 		
 	
