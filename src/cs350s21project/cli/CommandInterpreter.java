a68ed0d66e4915c85b54f9c54862d9c11b549008
Ryley Horton <rhorton5@eagles.ewu.edu>
2021-05-19 14:28:37 -0700

    Define sensor radar made and define ship made
    Ships with their munition IDs can now be defined as well as radar sensors.

--- src/cs350s21project/cli/CommandInterpreter.java
+++ src/cs350s21project/cli/CommandInterpreter.java
@@ -1,12 +1,21 @@
 package cs350s21project.cli;
 import java.util.ArrayList;
-import cs350s21project.controller.CommandManagers;
-import cs350s21project.controller.command.view.CommandViewCreateWindowTop;
-import cs350s21project.datatype.AgentID;
-import cs350s21project.datatype.Latitude;
-import cs350s21project.datatype.Longitude;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import cs350s21project.controller.*;
+import cs350s21project.controller.command.actor.CommandActorDefineShip;
+import cs350s21project.controller.command.munition.CommandMunitionDefineBomb;
+import cs350s21project.controller.command.munition.CommandMunitionDefineShell;
+import cs350s21project.controller.command.sensor.CommandSensorDefineRadar;
+import cs350s21project.controller.command.view.*;
+import cs350s21project.datatype.*;
 public class CommandInterpreter {
 
+	private AgentID id;
+	private int size;
+	private String objectType;
+	private String subType;
 	
 	private Latitude setLatitude(String str) {
 		String [] latData = str.split("[*#']");
@@ -18,12 +27,17 @@ public class CommandInterpreter {
 		return new Longitude(Integer.parseInt(lonData[0]), Integer.parseInt(lonData[1]),Double.parseDouble(lonData[2]));
 	}
 	
+	private FieldOfView setFieldOfView(String degree) {
+		AngleNavigational angleDegree = new AngleNavigational(Double.parseDouble(degree));
+		return new FieldOfView(angleDegree);
+	}
+	
 	public void evaluate(String commandText) {
 		String originalCommandText = commandText;
 		commandText=commandText.toLowerCase(); // lower case to allow any input in weird casing
 		commandText.trim(); //trim all whitespace
 		ArrayList <String> argumentList = new ArrayList<>();
-		while(!commandText.isEmpty()) {//Splits the string based on whitespaces.
+		while(!commandText.isEmpty()) {//Splits the string based on whitespace.
 			String temp =(commandText.contains(" ")) ? commandText.substring(0,commandText.indexOf(" ")+1).trim():commandText; //indexOf(" ")+1 will remove the whitespace at the start of the string.
 			temp = temp.replaceAll("[()]", ""); //Remove excess parameters.
 			argumentList.add(temp);
@@ -34,15 +48,16 @@ public class CommandInterpreter {
 		String commandType = argumentList.get(0);
 		CommandManagers cmd = CommandManagers.getInstance();
 		switch(commandType) {	
+		//--------Create Commands------------\\
 		case "create":
-			String objectType = argumentList.get(1);
+			objectType = argumentList.get(1);
 			switch(objectType) {
 			case "window":
-				AgentID id = new AgentID(argumentList.get(2));
+				id = new AgentID(argumentList.get(2));
 				String viewType = argumentList.get(3);
 				if(viewType.equals("top")) {
 					//index 4 and 5 are skipped since they hold arbitrary values ('with','view')
-					int size = Integer.parseInt(argumentList.get(6));
+					size = Integer.parseInt(argumentList.get(6));
 					Latitude latOrigin = setLatitude(argumentList.get(7));
 					Latitude latExtent = setLatitude(argumentList.get(8));
 					Latitude latInterval = setLatitude(argumentList.get(9));
@@ -58,11 +73,105 @@ public class CommandInterpreter {
 				}else 
 					throw new RuntimeException("Invalid view type for create window.");
 				break;
-				
-			}//End of switch(objectType)	
+			case "actor":
+				//TODO
+				break;
+			}//End of create objectType switch
+			break;
+		case "define":
+			objectType = argumentList.get(1);
+			switch(objectType) {
+			case "ship":
+				id = new AgentID(argumentList.get(2));
+				//index = 3, munition(s) = 4
+				List<AgentID> munitionList = argumentList.subList(5,argumentList.size()).stream().map(n -> new AgentID(n)).collect(Collectors.toList());
+				cmd.schedule(new CommandActorDefineShip(cmd,originalCommandText,id,munitionList));
+				System.out.print("Ship: " + id.getID() + " has been created with munitions: ");
+				munitionList.stream().forEach((n) -> System.out.print(n.getID() + " "));
+				System.out.println();
+				break;
+	//--------Munition Commands------------\\
+			case "munition":
+				subType = argumentList.get(2); //Types of munition, i.e. bomb,shell,charge
+				switch(subType) {
+				case "bomb":
+					id = new AgentID(argumentList.get(3));
+					cmd.schedule(new CommandMunitionDefineBomb(cmd,originalCommandText,id));
+					System.out.println(subType+" "+'"'+id.getID()+'"'+" created");
+					break;
+				case "shell":
+					System.out.println("Shell created");
+					cmd.schedule(new CommandMunitionDefineShell(cmd,originalCommandText,id));
+					System.out.println(subType+" "+'"'+id.getID()+'"'+" created");
+					break;
+				case "depth_charge":
+					//TODO
+					break;
+				case "missile":
+					//TODO
+					break;
+				case "torpedo":
+					//TODO
+					break;
+				}
+	//-------Sensor/Fuze Commands-----------\\
+			case "sensor":
+				subType = argumentList.get(2); //Types of sensors i.e. radar, thermal, acoustic
+				switch(subType) {
+				case "radar":
+					//TODO
+					id = new AgentID(argumentList.get(3));
+					//with = 4, field = 5, of = 6, view = 7
+					FieldOfView fov = this.setFieldOfView(argumentList.get(8));
+					//power = 9
+					Power power = new Power(Double.parseDouble(argumentList.get(10)));
+					//sensitivity = 11
+					Sensitivity sensitivity = new Sensitivity(Double.parseDouble(argumentList.get(12)));
+					cmd.schedule(new CommandSensorDefineRadar(cmd,originalCommandText,id,fov,power,sensitivity));
+					System.out.printf("Radar Sensor %s has been made.%n",id.getID());
+					break;
+				case"thermal":
+					//TODO
+					break;
+				case"acoustic":
+					//TODO
+					break;
+				case "sonar":
+					//TODO
+					break;
+				case "depth":
+					//TODO
+					break;
+				case "distance":
+					//TODO
+					break;
+				case "time":
+					//TODO
+					break;
+				}
+			} //End define commandType switch
+			break;
+	//---------MISC Commands-------------\\
+		case "delete":
+			//TODO
+			break;
+		case "set":
+			//TODO
+			break;
+		case "@load":
+			//TODO
+			break;
+		case "@wait":
+			//TODO
+			break;
+		case"@pause":
+			//TODO
+			break;
+		case "@set":
+			//TODO
+			break; 
 		}//End of switch(commandType)
 	}//End of evaluate()
 }
 		
 	
-
