b7c9e623f62d7e7d49465c626f87300c9d9156d7
AlexD4612 <59583079+AlexD4612@users.noreply.github.com>
2021-05-19 03:02:56 -0700

    Stubbed Commands, cleaned up previous code
    Stubbed out all commands, cleaned up previous code by adding class level fields for variables used multiple times, implemented "define bomb" and "define shell" commands to test if working.

--- src/cs350s21project/cli/CommandInterpreter.java
+++ src/cs350s21project/cli/CommandInterpreter.java
@@ -1,12 +1,16 @@
 package cs350s21project.cli;
 import java.util.ArrayList;
-import cs350s21project.controller.CommandManagers;
-import cs350s21project.controller.command.view.CommandViewCreateWindowTop;
-import cs350s21project.datatype.AgentID;
-import cs350s21project.datatype.Latitude;
-import cs350s21project.datatype.Longitude;
+import cs350s21project.controller.*;
+import cs350s21project.controller.command.munition.CommandMunitionDefineBomb;
+import cs350s21project.controller.command.munition.CommandMunitionDefineShell;
+import cs350s21project.controller.command.view.*;
+import cs350s21project.datatype.*;
 public class CommandInterpreter {
 
+	private AgentID id;
+	private int size;
+	private String objectType;
+	private String subType;
 	
 	private Latitude setLatitude(String str) {
 		String [] latData = str.split("[*#']");
@@ -23,7 +27,7 @@ public class CommandInterpreter {
 		commandText=commandText.toLowerCase(); // lower case to allow any input in weird casing
 		commandText.trim(); //trim all whitespace
 		ArrayList <String> argumentList = new ArrayList<>();
-		while(!commandText.isEmpty()) {//Splits the string based on whitespaces.
+		while(!commandText.isEmpty()) {//Splits the string based on whitespace.
 			String temp =(commandText.contains(" ")) ? commandText.substring(0,commandText.indexOf(" ")+1).trim():commandText; //indexOf(" ")+1 will remove the whitespace at the start of the string.
 			temp = temp.replaceAll("[()]", ""); //Remove excess parameters.
 			argumentList.add(temp);
@@ -35,14 +39,14 @@ public class CommandInterpreter {
 		CommandManagers cmd = CommandManagers.getInstance();
 		switch(commandType) {	
 		case "create":
-			String objectType = argumentList.get(1);
+			objectType = argumentList.get(1);
 			switch(objectType) {
 			case "window":
-				AgentID id = new AgentID(argumentList.get(2));
+				id = new AgentID(argumentList.get(2));
 				String viewType = argumentList.get(3);
 				if(viewType.equals("top")) {
 					//index 4 and 5 are skipped since they hold arbitrary values ('with','view')
-					int size = Integer.parseInt(argumentList.get(6));
+					size = Integer.parseInt(argumentList.get(6));
 					Latitude latOrigin = setLatitude(argumentList.get(7));
 					Latitude latExtent = setLatitude(argumentList.get(8));
 					Latitude latInterval = setLatitude(argumentList.get(9));
@@ -58,8 +62,88 @@ public class CommandInterpreter {
 				}else 
 					throw new RuntimeException("Invalid view type for create window.");
 				break;
-				
-			}//End of switch(objectType)	
+			case "actor":
+				//TODO
+				break;
+			}//End of create objectType switch
+			break;
+		case "define":
+			objectType = argumentList.get(1);
+			switch(objectType) {
+			case "ship":
+				//TODO
+				break;
+	//--------Munition Commands------------\\
+			case "munition":
+				subType = argumentList.get(2); //Types of munition, i.e. bomb,shell,charge
+				switch(subType) {
+				case "bomb":
+					id = new AgentID(argumentList.get(3));
+					cmd.schedule(new CommandMunitionDefineBomb(cmd,originalCommandText,id));
+					System.out.println(subType+" "+'"'+id.getID()+'"'+" created");
+					break;
+				case "shell":
+					id = new AgentID(argumentList.get(3));
+					cmd.schedule(new CommandMunitionDefineShell(cmd,originalCommandText,id));
+					System.out.println(subType+" "+'"'+id.getID()+'"'+" created");
+					break;
+				case "depth_charge":
+					//TODO
+					break;
+				case "missile":
+					//TODO
+					break;
+				case "torpedo":
+					//TODO
+					break;
+				}
+	//-------Sensor/Fuze Commands-----------\\
+			case "sensor":
+				subType = argumentList.get(2); //Types of sensors i.e. radar, thermal, acoustic
+				switch(subType) {
+				case "radar":
+					//TODO
+					break;
+				case"thermal":
+					//TODO
+					break;
+				case"acoustic":
+					//TODO
+					break;
+				case "sonar":
+					//TODO
+					break;
+				case "depth":
+					//TODO
+					break;
+				case "distance":
+					//TODO
+					break;
+				case "time":
+					//TODO
+					break;
+				}
+			} //End define commandType switch
+			break;
+	//---------MISC Commands-------------\\
+		case "delete":
+			//TODO
+			break;
+		case "set":
+			//TODO
+			break;
+		case "@load":
+			//TODO
+			break;
+		case "@wait":
+			//TODO
+			break;
+		case"@pause":
+			//TODO
+			break;
+		case "@set":
+			//TODO
+			break; 
 		}//End of switch(commandType)
 	}//End of evaluate()
 }
